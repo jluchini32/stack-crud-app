d78015b0611ecd8686ab4945bd78edae7372a854
Joe Luchini <joeluchini@Joes-MacBook-Pro.local>
2019-04-17 15:02:55 -0600

    create page and route done

--- controllers/sushi.js
+++ controllers/sushi.js
@@ -5,6 +5,26 @@ const Sushi  = require('../models/sushi');
 
 
 
+//new route
+router.post('/', (req, res) => {
+console.log(req.body,"<-- req.body will have the contents of the form");
 
+if(req.body.isRaw === 'on'){
+    req.body.isRaw = true;
+} else{
+    req.body.isRaw = false
+}
 
+Sushi.create(req.body, (err, createASushi) => {
+    if(err){
+        res.send(err);
+    } else {
+        res.redirect('/sushi');
+    }
+});
+});
+
+router.get('/new', (req, res) => {
+    res.render('new.ejs');
+   });
 module.exports = router; 
